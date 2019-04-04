3fc35f13c94621021d30aadbf13bc20cb6ae5536
Harris Brakmic <brakmic@gmail.com>
2019-04-04 18:36:31 +0200

    update links

--- src/client/scenes/layout.jsx
+++ src/client/scenes/layout.jsx
@@ -10,7 +10,7 @@ export class Layout extends React.Component {
           <Route path="/" component={GraphScene} />
         </div>
         <div className="footer">
-          Fork on <a href="https://github.com/altangent/lightning-viz">GitHub</a>
+          Fork on <a href="https://github.com/Actinium-project/lightning-viz">GitHub</a>
         </div>
       </div>
     );
